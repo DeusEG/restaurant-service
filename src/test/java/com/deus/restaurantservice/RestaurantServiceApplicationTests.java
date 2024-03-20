bdf908a705f53b17476befa85e1d23cd9b228806
Deus <nu_eg@mail.ru>
2024-03-20 11:44:37 +0300

    добавлена связь с бд и зависимости в pom

---
+++ src/test/java/com/deus/restaurantservice/RestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deus.restaurantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
