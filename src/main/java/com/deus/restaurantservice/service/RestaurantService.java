8bf292edf14907b974591b032f5239663b3664df
Deus <nu_eg@mail.ru>
2024-03-21 13:14:48 +0300

    Добавлен сервис ресторана

---
+++ src/main/java/com/deus/restaurantservice/service/RestaurantService.java
@@ -0,0 +1,20 @@
+package com.deus.restaurantservice.service;
+
+import com.deus.restaurantservice.model.Restaurant;
+import com.deus.restaurantservice.repository.ReservationRepository;
+import com.deus.restaurantservice.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public List<Restaurant> getAllUsers() {
+        return restaurantRepository.findAll();
+    }
+}
