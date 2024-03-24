7121dc9413a39d56d62e55b3dbd74fc92b827e11
Deus <nu_eg@mail.ru>
2024-03-24 17:30:39 +0300

    Добавлен контроллер для броинрования столов

---
+++ src/main/java/com/deus/restaurantservice/controller/ReservationController.java
@@ -0,0 +1,10 @@
+package com.deus.restaurantservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reservation")
+public class ReservationController {
+
+}
