c64b7e664722d61d3afceb9cc29c92352c5b9c17
Deus <nu_eg@mail.ru>
2024-03-31 21:35:51 +0300

    Добавлена навигация по эндпоинтам. Добавлены скрипты schema и data

---
+++ data.sql
@@ -0,0 +1,20 @@
+INSERT INTO role_data (name) VALUES 
+('MODER'),
+('USER'),
+('ADMIN');
+
+INSERT INTO user_data (name, password, telegram, id_role)
+VALUES
+('qqq', '$2a$10$H2u5Ljr9Xp2ACuVEbmjaLuLTsy99GjG36zoKjiiq0b0I0O8WIc4t6', 'qqq', 3),
+('qqq', '$2a$10$H2u5Ljr9Xp2ACuVEbmjaLuLTsy99GjG36zoKjiiq0b0I0O8WIc4t6', 'qqq1', 3),
+('aaa', '$2a$10$H2u5Ljr9Xp2ACuVEbmjaLuLTsy99GjG36zoKjiiq0b0I0O8WIc4t6', 'aaa', 2),
+('vvv', '$2a$10$kvzEnY8FWWMaww1IAbsTAuxxgKFgljU8pgZaVU1nIh9Bzc37HhowS', 'vvv', 1);
+
+INSERT INTO restaurant (address, id_admin)
+VALUES ('Первомайский проспект 131', 1),
+       ('Ленинского Комсомола 34', 2);
+	   
+INSERT INTO table_data (id_restaurant, number_of_seats)
+VALUES (1, 10),
+       (1, 4),
+       (2, 8);	   
